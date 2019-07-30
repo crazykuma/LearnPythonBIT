7ab4f6ffb46bced29753d60a1f2db815ba083179
crazykuma <crazykuma@qq.com>
2019-07-30 13:56:53 +0800

    更新1.2例题

---
+++ chapter1/examples/StarDraw.py
@@ -0,0 +1,12 @@
+"""
+绘制一个五角星
+"""
+from turtle import *
+color('red','red')
+begin_fill()
+for i in range(5):
+    fd(200)
+    rt(144)
+
+end_fill()
+done()
---
+++ chapter1/examples/TangentCirclesDraw.py
@@ -0,0 +1,11 @@
+"""实例2：同切圆绘制
+目标：绘制多个同切圆
+ps:代码在python123平台通过
+我的mac上只要运行turtle的库就会重启（不知道什么问题）
+"""
+import turtle
+turtle.pensize(2)
+turtle.circle(10)
+turtle.circle(40)
+turtle.circle(80)
+turtle.circle(160)
